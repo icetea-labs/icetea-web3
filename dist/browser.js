82beeb26f93ee463a54ed6dadde987ca3c5a1281
hoanghuy226 <hoanghuy226@gmail.com>
2019-03-26 16:27:09 +0700

    rebuild

--- dist/browser.js
+++ dist/browser.js
@@ -25573,6 +25573,7 @@ function () {
         throw new Error('Address ' + fromAddress + " don't exist in wallet");
       }
 
+      privateKey = 'CJUPdD38vwc2wMC3hDsySB7YQ6AFLGuU6QYQYaiSeBsK';
       return privateKey;
     }
   }, {
